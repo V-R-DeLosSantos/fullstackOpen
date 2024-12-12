272b1da9b7e6875ff99e89f7f5e5d2be18bebf09
V-R-DeLosSantos <valentino.r.delossantos@outlook.com>
2024-12-12 19:54:05 -0300

    1.4: Información del Curso paso 4

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,50 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>; 
+};
+
+const Part = (props) => {
+  return <p>{props.part.name} {props.part.exercises}</p>; 
+};
+
+const Content = (props) => {
+  return (
+    <>
+      {props.parts.map((part, index) => (
+        <Part key={index} part={part} />
+      ))}
+    </>
+  ); 
+}
+
+const Total = (props) => {
+  const totalExercises = props.parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Number of exercises {totalExercises}</p>; 
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
+
+  return (
+    <div>
+      <Header course={course} /> 
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default App;
