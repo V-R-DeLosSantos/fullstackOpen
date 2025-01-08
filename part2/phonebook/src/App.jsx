db6f49e92f5e268b44bc3f913c92e2a8eb066bc7
V-R-DeLosSantos <valentino.r.delossantos@outlook.com>
2025-01-08 12:08:06 -0300

    2.6: La Agenda Telefónica Paso 1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import Person from './components/Person'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const personsToShow = persons
+
+  return (
+    <>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {personsToShow.map(person =>
+          <Person key={person.name} person={person}/>
+        )}
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/components/Person.jsx
@@ -0,0 +1,7 @@
+const Person = ({ person }) => {
+    return (
+      <p style={{ margin: 0, lineHeight: '1.2' }}>{person.name}</p>
+    )
+  }
+  
+  export default Person
